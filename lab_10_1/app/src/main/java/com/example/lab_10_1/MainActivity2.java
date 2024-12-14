c4052ecd37cd5dba2c883becbaf4678510dfaa75
Ишмаев Руслан Ильдарович <ishmaev.ruslan@sogaz.ru>
2024-12-14 15:26:53 +0300

    lab 10, task 1

---
+++ lab_10_1/app/src/main/java/com/example/lab_10_1/MainActivity2.java
@@ -0,0 +1,43 @@
+package com.example.lab_10_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.lab_10_1.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        Button startButton = (Button) findViewById(R.id.second_button);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onclick(View view) {
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
